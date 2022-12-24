487d02dae165896613bfa72264a1561269ffd32f
shaown224 <60014974+shaown224@users.noreply.github.com>
2022-12-24 22:55:12 +0600

    Add files via upload

---
+++ upper_bound and lower_bound implemetation.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int upperr_bound(vector<int>&v,int x){
+	int L=0,R=int(v.size()-1),ans=int(v.size());
+
+	while(L<=R){
+		int mid = (L+R)>>1;
+		if(v[mid] > x){
+
+			ans=mid;
+			R  =mid -1;
+
+		}
+		else
+		{
+			L=mid+1;
+		}
+	}
+	return ans;
+}
+
+int lowerr_bound(vector<int>&v, int x){
+	int L=0,R=int(v.size()-1),ans=int(v.size());
+
+	while(L<=R){
+		int mid = (L+R)>>1;
+		if(v[mid] >= x){
+      
+      ans=mid;
+			R=mid-1;
+
+		}
+		else
+		{
+
+			L =mid +1;
+		}
+	}
+	return ans;
+}
+
+
+int main(){
+
+    vector<int> v ={3,3,5,5,7,7,7,7,7};
+
+    auto u= upperr_bound(v,7);
+    auto l= lowerr_bound(v,7);
+	  cout<<u<<l<<endl;
+
+    int ans = u-l;
+	  cout<<ans<<endl;
+	return 0;
+
+}
